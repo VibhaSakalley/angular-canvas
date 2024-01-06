fa92dc0e13e8083af85cdb74816020c187408bb5
SakalleyVibha <vibhasakalley.covetus@gmail.com>
2024-01-06 15:54:58 +0530

    worked on communicating data

--- projects/angular-canvas/src/lib/angular-canvas.component.ts
+++ projects/angular-canvas/src/lib/angular-canvas.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'lib-angular-canvas',
   template: `
-    <lib-test-view></lib-test-view>
+  <h3></h3>
+    <lib-test-view [hgt]="hgt" [wdt]="wdt" (onSaveImage)="onSaveImage($event)"></lib-test-view>
   `,
   styles: [
   ]
 })
 export class AngularCanvasComponent {
+
+  @Input() hgt : number | undefined;
+  @Input() wdt : number | undefined;
+  @Output() exportImg = new EventEmitter<string>();
+
+  onSaveImage(eve : any){
+    console.log('eve: ', eve);
+    this.exportImg.emit(eve);
+  }
 
 }
